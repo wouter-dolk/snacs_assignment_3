f3204f7aa212dc7fe35fb3a930499f8bf27e300c
wouter-dolk <wouterdolk@live.nl>
2022-12-13 13:25:51 +0100

    Add perturb experiment

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import math
 import random
 from statistics import mean
 import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def randomly_perturb_graph_v1(graph, percentage):
@@ -41,25 +43,97 @@ def randomly_perturb_graph_v2(graph, percentage):
     return perturbed_graph
 
 
-def print_statistics(graph):
-    closeness = nx.closeness_centrality(graph)
-    closeness_cent = mean(closeness.values())
+# TODO: custom perturbation
+# random perturb and then remove candidate set sizes of 1 and add them to others
 
+def print_statistics(graph):
     print('*** Statistics ***')
     print(f"Degree: what does the paper measure?")
     print(f"Diameter: {nx.diameter(graph)}")
     print(f"Avg. Shortest path length: {round(nx.average_shortest_path_length(graph), 3)}")
-    print(f"Avg. Closeness: {round(closeness_cent, 3)}")
+    print(f"Avg. Closeness: {round(mean(nx.closeness_centrality(graph).values()), 3)}")
     print(f"Avg. Betweenness: {round(mean(nx.betweenness_centrality(graph).values()), 3)}")
     print(f"Clust. Coeff.: {round(nx.average_clustering(graph), 3)}")
 
-# TODO: reindentification experiment
 
-# TODO: custom perturbation
+def candidate_set_dist(degree_hist):
+    candidate_sets = {
+        '1': 0,
+        '2-4': 0,
+        '5-10': 0,
+        '11-20': 0,
+        '20+': 0
+    }
+
+    for i in degree_hist:
+        if i == 1:
+            candidate_sets['1'] += i
+        elif 2 <= i <= 4:
+            candidate_sets['2-4'] += i
+        elif 5 <= i <= 10:
+            candidate_sets['5-10'] += i
+        elif 11 <= i <= 20:
+            candidate_sets['11-20'] += i
+        elif i > 20:
+            candidate_sets['20+'] += i
+
+    candidate_sets.update({k: v / sum(candidate_sets.values()) for k, v in candidate_sets.items()})
+    return candidate_sets
 
 
-G = nx.ring_of_cliques(10, 10)
-G_prime = randomly_perturb_graph_v2(G, 0.1)
+def h1_candidate_sets(graph):
+    degree_hist = nx.degree_histogram(graph)
+    return candidate_set_dist(degree_hist)
 
-print_statistics(G)
-print_statistics(G_prime)
+
+def perturbation_experiment(graph, name):
+    candidate_sets = [h1_candidate_sets(graph)]
+
+    for i in range(1, 11):
+        perturbed_graph = randomly_perturb_graph_v2(graph, i * 0.01)
+        candidate_sets.append(h1_candidate_sets(perturbed_graph))
+
+    x_axis = [x for x in range(0, 11)]
+    y1 = np.array([])
+    y2 = np.array([])
+    y3 = np.array([])
+    y4 = np.array([])
+
+    for candidate_set in candidate_sets:
+        y1 = np.append(y1, candidate_set['1'])
+        y2 = np.append(y2, candidate_set['2-4'])
+        y3 = np.append(y3, candidate_set['5-10'])
+        y4 = np.append(y4, candidate_set['11-20'])
+
+    # Stack the candidate set proportions
+    y2 = y1 + y2
+    y3 = y2 + y3
+    y4 = y3 + y4
+
+    # Plot reinditification using H1
+    fig, ax = plt.subplots()
+    ax.fill_between(x_axis, y1, color='blue', alpha=1)
+    ax.fill_between(x_axis, y1, y2, color='blue', alpha=0.6)
+    ax.fill_between(x_axis, y2, y3, color='blue', alpha=0.3)
+    ax.fill_between(x_axis, y4, y3, color='blue', alpha=0.2)
+    plt.xticks([0, 5, 10], ['0%', '5%', '10%'])
+    plt.yticks([0.00, 0.20, 0.40, 0.60, 0.80, 1.00])
+    plt.margins(x=0, y=0)
+    plt.xlabel('Perturbation')
+    plt.ylabel('Node proporion')
+    plt.title(f'{name}')
+    plt.show()
+
+    # Statistics for 0, 5 10 and 100% perturbation
+    print_statistics(graph)
+    print_statistics(randomly_perturb_graph_v2(graph, 0.05))
+    print_statistics(randomly_perturb_graph_v2(graph, 0.1))
+    print_statistics(randomly_perturb_graph_v2(graph, 1))
+
+
+# G = nx.read_edgelist('data/CEOclubmembership.csv', delimiter=',', nodetype=int, encoding="utf-8")
+# G = nx.ring_of_cliques(10, 10)
+G = nx.les_miserables_graph()
+perturbation_experiment(G, 'CEO club membership')
+
+
